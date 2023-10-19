6835fcfd932836ae0c4865013623cd2516347110
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2023-10-18 23:35:36 -0500

    Initial Commit

---
+++ SwiftUIComponentsFramework/SwiftUIComponentsFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIComponentsFramework.h
+//  SwiftUIComponentsFramework
+//
+//  Created by Julian Pineda on 2023-10-18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIComponentsFramework.
+FOUNDATION_EXPORT double SwiftUIComponentsFrameworkVersionNumber;
+
+//! Project version string for SwiftUIComponentsFramework.
+FOUNDATION_EXPORT const unsigned char SwiftUIComponentsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIComponentsFramework/PublicHeader.h>
+
+
